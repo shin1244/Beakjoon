835360b7e2e81a3cf78018603487c744de645c0d
shin1244 <126842353+shin1244@users.noreply.github.com>
2024-01-12 11:10:43 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+def Beakjoon(num):
+    if num >= 90:
+        print('A')
+    elif num >= 80:
+        print('B')
+    elif num >= 70:
+        print('C')
+    elif num >= 60:
+        print('D')
+    else:
+        print('F')
+
+A  = int(input())
+Beakjoon(A)

a3545ebc63d9410826e4424d7146d1a4160e26f6
신준규 <126842353+shin1244@users.noreply.github.com>
2024-01-24 20:09:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 1120 ms, Memory: 122228 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,22 +1,22 @@
 import sys
-from collections import deque, defaultdict
-input = sys.stdin.readline
-total, n = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
-arr_dict = defaultdict(list)
-for key1, key2 in arr:
-    arr_dict[key1].append(key2)
-    arr_dict[key2].append(key1)
-visited = set()
-component_count = 0
-for i in range(1, total + 1):
-    if i not in visited:
-        if i in arr_dict:
-            q = deque([i])
-            while q:
-                now = q.popleft()
-                if now not in visited:
-                    q.extend(arr_dict[now])
-                    visited.add(now)
-        component_count += 1
-print(component_count)
+input = sys.stdin.readline 
+nodes, edges = list(map(int,input().split()))
+
+arr = [list(map(int,input().split())) for _ in range(edges)]
+
+link_dict = {i:i for i in range(1,nodes+1)}
+
+def find(node):
+    if link_dict[node] != node:
+        link_dict[node] = find(link_dict[node])
+    return link_dict[node]
+
+def union(node1, node2):
+    root1, root2 = find(node1), find(node2)
+    if root1 != root2:
+        link_dict[max(root1, root2)] = min(root1, root2)
+
+for i, j in arr:
+    union(i, j)
+
+print(len(set(find(node) for node in link_dict)))

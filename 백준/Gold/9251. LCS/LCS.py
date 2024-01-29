effaa34d440ae223bb648a472d7f280d64773992
신준규 <126842353+shin1244@users.noreply.github.com>
2024-01-29 21:49:10 +0900

    [Gold V] Title: LCS, Time: 568 ms, Memory: 55708 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,13 @@
+s1 = input()
+s2 = input()
+
+DP = [[0] *(len(s2)+1) for _ in range(len(s1)+1)]
+
+for i in range(1, len(s1)+1):
+    for j in range(1, len(s2)+1):
+        if s1[i-1] == s2[j-1]:
+            DP[i][j] = DP[i-1][j-1] + 1
+        else:
+            DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+        
+print(DP[-1][-1])

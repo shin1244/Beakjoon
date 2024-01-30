1efb7a0fb18a6a89ef6fb8032e7fe974181cf4dc
신준규 <126842353+shin1244@users.noreply.github.com>
2024-01-30 11:10:20 +0900

    [Gold V] Title: 동전, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -1,18 +1,15 @@
-n = int(input())
+N = int(input())
 
-for _ in range(n):
-
-    
-    now_n = int(input())
-    arr = list(map(int,input().split(' ')))
+for _ in range(N):
+    input()
+    coins = map(int,input().split())
     money = int(input())
 
-    DP = [0] * (money + 1)
+    DP = [0] * (money+1)
     DP[0] = 1
 
-    for i in arr:
-        for j in range(1,money+1):
-            if j >= i:
-                DP[j] += DP[j-i]
-
+    for coin in coins:
+        for i in range(coin, len(DP)):
+            DP[i] += DP[i-coin]
+    
     print(DP[-1])

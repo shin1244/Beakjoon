c011a1f862205e54cd28254f16da0a85cfdea310
신준규 <126842353+shin1244@users.noreply.github.com>
2024-03-15 15:40:12 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -2,11 +2,13 @@ n = int(input())
 
 result = []
 while n != 1:
-    for i in range(2,n+1):
+    c = 1
+    for i in range(2,int(n**(0.5))+1):
         if n % i == 0:
-            result.append(i)
+            print(i)
             n = n//i
+            c = 0
             break
-
-for i in result:
-    print(i)
+    if c:
+        print(n)
+        n = 1

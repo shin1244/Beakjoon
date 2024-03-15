6a49ff82f027f455c31c3e989c451e15ac3f47ff
신준규 <126842353+shin1244@users.noreply.github.com>
2024-03-15 16:45:40 +0900

    [Silver IV] Title: 스택 2, Time: 1396 ms, Memory: 269828 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [input().rstrip().split(" ") for _ in range(n)]
+
+result = []
+
+for i in arr:
+    if i[0] == '1':
+        result.append(i[1])
+    elif i[0] == '2':
+        if result: print(result.pop())
+        else: print(-1)
+    elif i[0] == '3':
+        print(len(result))
+    elif i[0] == '4':
+        print(0) if len(result) else print(1)
+    else:
+        if result: print(result[-1])
+        else: print(-1)

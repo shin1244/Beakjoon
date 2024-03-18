c5a419c17ac7426ea2a8b7914849a7d56900a3d5
신준규 <126842353+shin1244@users.noreply.github.com>
2024-03-18 12:57:35 +0900

    [Silver IV] Title: 듣보잡, Time: 92 ms, Memory: 43924 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,13 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int,input().split(' '))
 
 S1 = set()
 S2 = set()
 
 for i in range(n):
-    S1.add(input())
+    S1.add(input().rstrip())
 
 for j in range(m):
-    S2.add(input())
+    S2.add(input().rstrip())
 
 arr = list(S1&S2)
 

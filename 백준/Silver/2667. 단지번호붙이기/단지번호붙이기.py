f5a7769a377886cfcc7f6770801108b9ebcc88e9
신준규 <126842353+shin1244@users.noreply.github.com>
2024-03-20 21:50:23 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -11,12 +11,27 @@ def DFS(b, a, count):
                 count = DFS(y+b, x+a, count)
     return count
 
-result = []
+def BFS(b, a, count):
+    stack = [[b, a]]
+    while stack:
+        yy, xx =  stack.pop(0)
+        if arr[yy][xx] != 1:
+            continue
+        arr[yy][xx] = 0
+        count += 1
+        for y, x in [[1,0],[0,1],[0,-1],[-1,0]]:
+            if yy+y in range(n) and xx+x in range(n):
+                if arr[yy+y][xx+x] == 1:
+                    stack.append([yy+y, xx+x])
+    return count
 
+result = []
+c = -1
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
-            result.append(DFS(i, j, 1))
+            c -=1
+            result.append(BFS(i, j, 0))
 
 print(len(result))
 for i in sorted(result):

02a0df1512348d041f3bf08c23f7ad49464f909c
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-01 00:01:59 +0900

    [Silver I] Title: 숨바꼭질, Time: 3260 ms, Memory: 132236 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+start, end = map(int, input().split(' '))
+
+DP = set()
+
+q = deque([[0, start]])
+
+while q:
+    cnt, now = q.popleft()
+    DP.add(now)
+
+    if now == end:
+        print(cnt)
+        break
+
+    if now < 100000 and now+1 not in DP:
+        q.append([cnt+1, now+1])
+    if now > 0 and now-1 not in DP:
+        q.append([cnt+1, now-1])
+    if now < 500000 and now*2 not in DP:
+        q.append([cnt+1, now*2])

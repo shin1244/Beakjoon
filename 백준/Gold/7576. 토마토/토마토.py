42f01b806d48b76fecf0e0a429c5046eac46a2c5
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-01 14:59:59 +0900

    [Gold V] Title: 토마토, Time: 4660 ms, Memory: 110996 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+width, length = map(int,input().split(' '))
+
+arr = [list(map(int,input().split(' '))) for _ in range(length)]
+tomato = deque()
+sample = []
+cnt = 0
+
+for y in range(length):
+    for x in range(width):
+        if arr[y][x] == 1:
+            tomato.append([y, x])
+
+while tomato:
+    y, x = tomato.popleft()
+    for ny, nx in [[1,0],[0,1],[-1,0],[0,-1]]:
+        if y+ny in range(length) and x+nx in range(width):
+            if arr[y+ny][x+nx] == 0:
+                arr[y+ny][x+nx] = 1
+                sample.append([y+ny, x+nx])
+
+    if not tomato:
+        cnt += 1
+        tomato.extend(sample)
+        sample = []
+
+for i in arr:
+    if 0 in i:
+        print(-1)
+        exit()
+print(cnt-1)

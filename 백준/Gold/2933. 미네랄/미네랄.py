1fde0e106ea5a66fe25dc148ac09e09eae3121e1
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-02 19:03:01 +0900

    [Gold I] Title: 미네랄, Time: 1072 ms, Memory: 34176 KB -BaekjoonHub

---
+++ 백준/Gold/2933. 미네랄/미네랄.py
@@ -0,0 +1,70 @@
+from collections import deque
+
+n, m = map(int,input().split())
+arr = [list(input()) for _ in range(n)]
+input()
+bars = list(map(int,input().split(' ')))
+
+
+def BFS(y, x):
+    if y == n-1:
+        return True
+    visited.add((y,x))
+    q = deque([[y,x]])
+    while q:
+        now_y, now_x = q.popleft()
+        if now_y == n-1:
+            return False
+        for ny, nx in [[0,-1],[0,1],[-1,0],[1,0]]:
+            next_y, next_x = now_y+ny, now_x+nx
+            if 0 <= next_y < n and 0 <= next_x < m:
+                if arr[next_y][next_x] == 'x' and (next_y, next_x) not in visited:
+                    visited.add((next_y, next_x))
+                    q.append([next_y, next_x])
+    return visited
+
+
+def down(s):
+    dic = {}
+    for y, x in s:
+        if x in dic:
+            dic[x].append(y)
+        else:
+            dic[x] = [y]
+    min_length = 999
+    for x in dic:
+        c = 1
+        for i in dic[x]:
+            for y in range(i+1, n):
+                if arr[y][x] == 'x' and (y, x) not in s:
+                    min_length = min(min_length, y-i-1)
+                    c = 0
+                    break
+            if c:
+                min_length = min(min_length, n-i-1)
+    result = set()
+    for y, x in s:
+        arr[y][x] = '.'
+        result.add((y+min_length, x))
+    for y, x in result:
+        arr[y][x] = 'x'
+
+
+for bar in range(len(bars)):
+    now = n-bars[bar]
+    repeat = range(m) if bar % 2 == 0 else range(m-1, -1, -1)
+    for i in repeat:
+        if arr[now][i] == 'x':
+            arr[now][i] = '.'
+            for y, x in [[0,-1],[0,1],[-1,0],[1,0]]:
+                next_y, next_x = now+y, i+x
+                if 0 <= next_y < n and 0 <= next_x < m:
+                    if arr[next_y][next_x] == 'x':
+                        visited = set()
+                        fall = BFS(next_y, next_x)
+                        if type(fall) == set:
+                            down(fall)
+            break
+
+for i in arr:
+    print(''.join(i))

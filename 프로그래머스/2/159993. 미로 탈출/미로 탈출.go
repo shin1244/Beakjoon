ccbd514f746da4a247c44bfba822597c03928753
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-09 16:11:52 +0900

    [level 2] Title: 미로 탈출, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.go
@@ -0,0 +1,67 @@
+import (
+	"reflect"
+)
+
+func inVisited(visited [][]int, now []int) bool {
+	for _, val := range visited {
+		if reflect.DeepEqual(val, now) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func BFS(y, x, l int, maps []string) [][]int {
+	n := len(maps)
+	m := len(maps[0])
+	q := [][]int{{0, y, x}}
+	visited := make([][]int, n)
+	for i := range visited {
+		visited[i] = make([]int, m)
+	}
+
+	for len(q) > 0 {
+		cnt, nowY, nowX := q[0][0], q[0][1], q[0][2]
+		q = q[1:]
+		if l == 1 && string(maps[nowY][nowX]) == "L" {
+			return [][]int{{cnt}, {nowY, nowX}}
+		}
+		if l == 0 && string(maps[nowY][nowX]) == "E" {
+			return [][]int{{cnt}, {nowY, nowX}}
+		}
+		for _, next := range [][]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
+			nextY, nextX := next[0]+nowY, next[1]+nowX
+			nowCnt := cnt + 1
+			if 0 <= nextY && nextY < n && 0 <= nextX && nextX < m {
+				if string(maps[nextY][nextX]) != "X" && visited[nextY][nextX] == 0 {
+					q = append(q, []int{nowCnt, nextY, nextX})
+					visited[nextY][nextX] = 1
+				}
+			}
+		}
+	}
+	return [][]int{{-1}, {-1}}
+}
+
+func solution(maps []string) int {
+	result := 0
+	for y, width := range maps {
+		for x, now := range width {
+			if now == 'S' {
+				bfsResult := BFS(y, x, 1, maps)
+				if bfsResult[0][0] == -1 {
+					return -1
+				}
+				result += bfsResult[0][0]
+				bfsResult = BFS(bfsResult[1][0], bfsResult[1][1], 0, maps)
+				if bfsResult[0][0] == -1 {
+					return -1
+				}
+				result += bfsResult[0][0]
+				return result
+			}
+		}
+	}
+	return -1
+}

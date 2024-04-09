4ffb7ee39847c61361351902eb862d99cbc9d1a1
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-09 16:18:15 +0900

    [level 2] Title: 미로 탈출, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.go
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.go
@@ -1,17 +1,3 @@
-import (
-	"reflect"
-)
-
-func inVisited(visited [][]int, now []int) bool {
-	for _, val := range visited {
-		if reflect.DeepEqual(val, now) {
-			return false
-		}
-	}
-
-	return true
-}
-
 func BFS(y, x, l int, maps []string) [][]int {
 	n := len(maps)
 	m := len(maps[0])

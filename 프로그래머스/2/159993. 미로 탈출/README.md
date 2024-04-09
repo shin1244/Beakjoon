22eca8a3485c267bda3b270560e0de96073801f0
신준규 <126842353+shin1244@users.noreply.github.com>
2024-04-09 16:18:06 +0900

    [level 2] Title: 미로 탈출, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/README.md
+++ 프로그래머스/2/159993. 미로 탈출/README.md
@@ -1,10 +1,10 @@
 # [level 2] 미로 탈출 - 159993 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/159993?language=python3) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.01 ms
+메모리: 10.3 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 09일 16:11:51
+2024년 04월 09일 16:18:06
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,38 @@
+from collections import deque
+
+def solution(maps):
+    result = 0
+    for y in range(len(maps)):
+        for x in range(len(maps[y])):
+            if maps[y][x] == 'S':
+                count, now = BFS(y, x, maps, 1)
+                if count == -1:
+                    return -1
+                result += count
+                count, now = BFS(now[0], now[1], maps, 0)
+                if count == -1:
+                    return -1
+                result += count
+                return result
+
+
+def BFS(y, x, maps, L):
+    n = len(maps)
+    m = len(maps[0])
+    q = deque([[0,y,x]])
+    visited = set()
+
+    while q:
+        cnt, now_y, now_x = q.popleft()
+        if L == 1 and maps[now_y][now_x] == 'L':
+            return (cnt, [now_y, now_x])
+        if L == 0 and maps[now_y][now_x] == 'E':
+            return (cnt, [now_y, now_x])
+        for ny, nx in [[1,0],[-1,0],[0,1],[0,-1]]:
+            next_y, next_x = ny+now_y, nx+now_x
+            if 0 <= next_y < n and 0 <= next_x < m:
+                if maps[next_y][next_x] != 'X' and (next_y, next_x) not in visited:
+                    q.append([cnt+1, next_y, next_x])
+                    visited.add((next_y, next_x))
+    
+    return (-1, -1)

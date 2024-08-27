88a2cae06ba244bc68d094becc493a674af55439
신준규 <126842353+shin1244@users.noreply.github.com>
2024-08-27 16:30:22 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 257.11 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -1,30 +1,24 @@
-from collections import deque
-
 def solution(land):
-    row = len(land[0])
-    column = len(land)
-    DP = [0] * row
-    for x in range(row):
-        for y in range(column):
-            if land[y][x] == 1:
-                q = deque([[y,x]])
-                visited = set()
-                count = 0
-                while q:
-                    now_y, now_x = q.popleft()
-                    visited.add((now_y, now_x))
-                    count += 1
-                    
-                    for ny, nx in [[0, 1], [0, -1], [1, 0], [-1, 0]]:
-                        new_y, new_x = now_y + ny, now_x + nx
-                        if 0 <= new_y < column and 0 <= new_x < row:
-                            if (new_y, new_x) not in visited and land[new_y][new_x] == 1:
-                                land[new_y][new_x] = 0
-                                q.append([new_y, new_x])
-                row_check = set()
-                for _, vx in visited:
-                    row_check.add(vx)
-                for check in row_check:
-                    DP[check] += len(visited)
-                    
-    return max(DP)
+    n, m = len(land), len(land[0])
+    visited = [[True]*m for _ in range(n)]
+    delta = [(1,0),(-1,0),(0,1),(0,-1)]
+    oil_cnt = [0]*m
+    for i in range(n):
+        for j in range(m):
+            if land[i][j] and visited[i][j]:
+                visited[i][j] = False
+                s = [(i,j)]
+                col = set()
+                oil = 0
+                while s:
+                    x, y = s.pop()
+                    col.add(y)
+                    oil += 1
+                    for dx, dy in delta:
+                        X, Y = x+dx, y+dy
+                        if 0<=X<n and 0<=Y<m and land[X][Y] and visited[X][Y]:
+                            visited[X][Y] = False
+                            s.append((X,Y))
+                for y in col:
+                    oil_cnt[y] += oil
+    return max(oil_cnt)

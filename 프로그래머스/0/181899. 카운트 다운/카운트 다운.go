3e30ae416b675317d545b9f71e28d5826696db1d
신준규 <126842353+shin1244@users.noreply.github.com>
2025-03-01 02:11:13 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 3.55 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.go
@@ -0,0 +1,7 @@
+func solution(start_num int, end_num int) []int {
+    result := []int{}
+    for i := start_num; i >= end_num; i-- {
+        result = append(result, i)
+    }
+    return result
+}
